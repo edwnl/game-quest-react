192e17821bd6712f1c20c8e6e23b1ca48aae47de
Ed <edwinli123456@gmail.com>
2023-12-24 01:02:41 +1100

    Added comments to custom hook 💡

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -16,17 +16,22 @@ const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setErrors] = useState("");
 
+  // useEffect used to fetch games when the component mounts.
   useEffect(() => {
+    // Used to abort the API request when needed.
     const controller = new AbortController();
 
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
       .then((res) => setGames(res.data.results))
       .catch((err) => {
+        // If the error is caused by the AbortController, do nothing.
         if (err instanceof CanceledError) return;
         setErrors(err.message);
       });
 
+    // Clean up function: Abort the ongoing API request when the component unmounts.
+    // The empty dependency array ensures that this effect runs only once when the component mounts.
     return () => controller.abort();
   }, []);
 
