3cb43de022943a96a44e9be168cb31fd346a865a
Ed <edwinli123456@gmail.com>
2023-12-24 04:01:59 +1100

    Adding loading skeletons for cards 🚧

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,19 +24,25 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setErrors] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   // useEffect used to fetch games when the component mounts.
   useEffect(() => {
     // Used to abort the API request when needed.
     const controller = new AbortController();
 
+    setLoading(true);
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
         // If the error is caused by the AbortController, do nothing.
         if (err instanceof CanceledError) return;
         setErrors(err.message);
+        setLoading(false);
       });
 
     // Clean up function: Abort the ongoing API request when the component unmounts.
@@ -47,6 +53,7 @@ const useGames = () => {
   return {
     games,
     error,
+    isLoading,
   };
 };
 
