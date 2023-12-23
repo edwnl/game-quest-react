64222849d102d53ff50d6a94860380ee43a44aee
Ed <edwinli123456@gmail.com>
2023-12-24 03:48:51 +1100

    Adding critic score badge 🚧

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,5 +1,5 @@
 import { Platform } from "../hooks/useGames.ts";
-import { HStack, Icon, Text } from "@chakra-ui/react";
+import { HStack, Icon } from "@chakra-ui/react";
 import {
   FaApple,
   FaLinux,
