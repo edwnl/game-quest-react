dec4b3e1ae4065094d92aa68c659ff0e11d22b5c
Ed <edwinli123456@gmail.com>
2024-01-02 20:21:16 +0800

    Display Genres with Images 🚧

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,25 @@
 import useGenres from "../hooks/useGenres.ts";
+import {HStack, Image, List, ListItem, Text} from "@chakra-ui/react";
+import getCroppedImageUrl from "../services/image-url.ts";
 
 const GenreList = () => {
     const {data} = useGenres();
 
+    // List element allows rendering a list without a bullet
+    // point like <li>
     return (
-        <ul>
-            {data.map(data => <li key={data.id}>{data.name}</li>)}
-        </ul>
+        <List>
+            {data.map(data =>
+                <ListItem paddingY='5px' key={data.id}>
+                    <HStack>
+                        <Image
+                            boxSize='32px'
+                            borderRadius={8}
+                            src={getCroppedImageUrl(data.image_background)} />
+                        <Text fontSize='lg'>{data.name}</Text>
+                    </HStack>
+                </ListItem>)}
+        </List>
     );
 };
 
