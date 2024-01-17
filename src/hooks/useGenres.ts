f5fdbedb73aaf196820e36866b23c195f09d0cc3
Ed <edwinli123456@gmail.com>
2024-01-17 23:47:44 +1100

    Ship genres as static data 🗃️

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,13 +1,19 @@
-import useData from "./useData.ts";
+import genres from "../data/genres.ts";
 
 export interface Genre {
-    id: number;
-    name: string;
-    slug: string;
-    games_count: number;
-    image_background: string;
+  id: number;
+  name: string;
+  slug: string;
+  games_count: number;
+  image_background: string;
 }
 
-const useGenres = () => useData<Genre>("/genres");
+// const useGenres = () => useData<Genre>("/genres");
+
+const useGenres = () => ({
+  data: genres,
+  isLoading: false,
+  error: null,
+});
 
 export default useGenres;
